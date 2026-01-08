3414593b449ecb4a9d84a67e7f0037ecbb7595c0
Pavan <platinumpavan123@gmail.com>
2026-01-08 16:34:42 -0500

    Controller Setup

---
+++ server/src/main/java/com/prediction/market/prediction_market/controllers/MarketsController.java
@@ -0,0 +1,10 @@
+package com.prediction.market.prediction_market.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/markets")
+public class MarketsController {
+
+}
---
+++ server/src/main/java/com/prediction/market/prediction_market/controllers/UsersController.java
@@ -0,0 +1,10 @@
+package com.prediction.market.prediction_market.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/users")
+public class UsersController {
+
+}
